2ca16c937d3c61ef5a34412994696116e7d49ce9
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-17 17:01:47 +0100

    Update Card-objects.jsx

--- src/components/card-objects/Card-objects.jsx
+++ src/components/card-objects/Card-objects.jsx
@@ -53,3 +53,33 @@ const CardObjects = ({
 };
 
 export default CardObjects;
+
+// Object-Products.jsx
+// const ObjectProducts = ({ images, product }) => {
+// 	return (
+// 		<StyledDivProduct>
+// 			<StyledImg src={images.mobile} />
+// 			<StyledText>{product}</StyledText>
+// 		</StyledDivProduct>
+// 	);
+// };
+
+// export default ObjectProducts;
+
+// object-products.styles.js------
+// import styled from 'styled-components';
+// import { COLORS } from '../../styles/colors';
+
+// const StyledDivProduct = styled.div`
+// 	padding: 26px 24px 24px;
+// `;
+
+// const StyledImg = styled.img`
+// 	border: 1px solid ${COLORS.red};
+// `;
+
+// const StyledText = styled.p`
+// 	color: red;
+// `;
+
+// export { StyledDivProduct, StyledImg, StyledText };
